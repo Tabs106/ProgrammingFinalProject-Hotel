c7ac5f1a47d5b34f0f886c6e09b6df6c687a47fb
correat <tabathacorrea@hotmail.com>
2023-01-21 16:50:42 +0100

    First draft

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,12 @@
 #by Hassanat Awodipe, Tabatha Correa and Tamsir Jobe
+
+#This section reads and cleans the given dataset, removing lines where it finds  blanks in important 
+# information.
+
+import pandas as pd
+
+#reads the csv
+df = pd.read_csv('salaries1.csv')
+
+#drops all rows that contain null variables and sets to second var
+df2 = df.dropna()
