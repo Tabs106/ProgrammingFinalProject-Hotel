1a2b3f8829c786d40db5f3211fc713ae632ea9d1
Tjobe01 <91953538+Tjobe01@users.noreply.github.com>
2023-02-19 13:46:59 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,1 +1,20 @@
 #by Hassanat Awodipe, Tabatha Correa and Tamsir Jobe
+import pandas as pd
+
+from utils import Countries, Families
+
+def analyse_country(data):
+    country = Countries(data)
+    country.plot_top_countries('Top_10_countries.pdf')
+    country.csv_saver('Top_10_countries.csv')
+    
+def analyse_families(data):
+    family = Families(data)
+    family.plot_families('Families_or_not.pdf')
+    family.csv_saver('Families_or_not.csv')
+    
+if __name__ == '__main__':
+    hotel_data = pd.read_csv(r'hotel_bookings.csv', encoding='utf-8')
+    analyse_country(hotel_data)
+    analyse_families(hotel_data)
+    
